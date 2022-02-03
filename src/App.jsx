fb19793b62ab5715425549b742ef0c68e3bb32d2
Omer Karakus <omerfarukkarakus01@gmail.com>
2022-02-03 10:15:18 +0000

    change the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect, createContext } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
-import { Features } from "./components/features";
+// import { Features } from "./components/features";
 import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
+// import { Services } from "./components/services";
+// import { Gallery } from "./components/gallery";
+// import { Testimonials } from "./components/testimonials";
 // import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
-import Component1 from "./components/Component1";
-import Component2 from "./components/Component2";
+// import Component1 from "./components/Component1";
+// import Component2 from "./components/Component2";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -27,33 +27,33 @@ const App = () => {
     setLandingPageData(JsonData);
   }, []);
 
-  const [people, setPeople] = useState([]);
+  // const [people, setPeople] = useState([]);
 
-  const addToTable = (payload) => {
-    setPeople((prevState) => {
-      const tmpPeople = [...prevState];
-      tmpPeople.push(payload);
-      tmpPeople.sort((a, b) => new Date(a.birth) - new Date(b.birth));
-      return tmpPeople;
-    });
-  };
+  // const addToTable = (payload) => {
+  //   setPeople((prevState) => {
+  //     const tmpPeople = [...prevState];
+  //     tmpPeople.push(payload);
+  //     tmpPeople.sort((a, b) => new Date(a.birth) - new Date(b.birth));
+  //     return tmpPeople;
+  //   });
+  // };
 
   return (
     <div>
       <div>
         <Navigation />
         <Header data={landingPageData.Header} />
-        <div style={{ height: 200 }}>
+        {/* <div style={{ height: 200 }}>
           <PeopleContext.Provider value={{ addToTable, people }}>
             <Component1 />
             <Component2 />
           </PeopleContext.Provider>
-        </div>
-        <Features data={landingPageData.Features} />
+        </div> */}
+        {/* <Features data={landingPageData.Features} /> */}
+        {/* <Services data={landingPageData.Services} /> */}
         <About data={landingPageData.About} />
-        <Services data={landingPageData.Services} />
-        <Gallery data={landingPageData.Gallery} />
-        <Testimonials data={landingPageData.Testimonials} />
+        {/* <Gallery data={landingPageData.Gallery} /> */}
+        {/* <Testimonials data={landingPageData.Testimonials} /> */}
         {/* <Team data={landingPageData.Team} /> */}
         <Contact data={landingPageData.Contact} />
       </div>
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -33,17 +33,17 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-            <li>
+            {/* <li>
               <a href="#features" className="page-scroll">
                 Features
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#services" className="page-scroll">
                 Services
               </a>
@@ -57,7 +57,7 @@ export const Navigation = (props) => {
               <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
-            </li>
+            </li> */}
             {/* <li>
               <a href='#team' className='page-scroll'>
                 Team
